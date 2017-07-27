98037b47fb165f98604c6c09c06e9e11ed12daf3
Sara Sulit & Sofia Forsell & Steven Yuen <pair+sarayapsulit.sforsell.steveny91@devbootcamp.com>
2017-07-27 12:14:20 -0400

    Corrected pm table

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,6 @@ class Question < ActiveRecord::Base
   belongs_to :user
   validates :title, :text, presence: true
   validates :user
+  has_many :votes, as: :voteable
+  has_many :comments, as: :commentable
 end
