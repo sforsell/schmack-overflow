76808f815ea2a73893dfc8771f7fc904d6abd8e2
Sara Sulit & Sofia Forsell & Steven Yuen <pair+sarayapsulit.sforsell.steveny91@devbootcamp.com>
2017-07-27 18:15:44 -0400

    method for score

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,8 @@ class Question < ActiveRecord::Base
   has_many :comments, as: :commentable
 
   validates :title, :question, :user, presence: true
+
+  def score
+    self.votes.where(vote: true).count - self.votes.where(vote: false).count
+  end
 end
