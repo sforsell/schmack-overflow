3a8a44385fd3eaf10bac15ebfc96da87efe92211
Sara Sulit & Sofia Forsell & Steven Yuen <pair+sarayapsulit.sforsell.steveny91@devbootcamp.com>
2017-07-27 11:38:47 -0400

    Make user

---
+++ app/helpers/user-login.rb
@@ -0,0 +1,11 @@
+def current_user
+  @current_user ||= User.find_by(id: session[:user_id])
+end
+
+def login?
+  !!current_user
+end
+
+def require_user
+  redirect '/session/new' unless login?
+end
