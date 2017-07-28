97ab1790760bd2d19210708aa8267d7688a0e8ff
Sara Sulit & Sofia Forsell & Steven Yuen <pair+sarayapsulit.sforsell.steveny91@devbootcamp.com>
2017-07-28 00:30:48 -0400

    Add new answwer forms and vote buttons

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,17 +1,20 @@
 post '/questions/:id/votes' do
-  @question = Question.find(params[:id])
-  votes = Vote.create(user: current_user, votable_id: params[:id], votable_type: "Question", vote: params[:vote])
-  redirect "/questions/#{ @question.id}"
+  question = Question.find(params[:id])
+  vote = Vote.create(user: current_user, votable_id: params[:id], votable_type: "Question", vote: boolean(params[:vote]))
+    p params[:vote]
+  redirect "/questions/#{ question.id}"
 end
 
 post '/answers/:id/votes' do
-  @answer = Answer.find(params[:id])
-  votes = Vote.create(user: current_user, votable_id: params[:id], votable_type: "Answer", vote: params[:vote])
-  redirect "/answers/#{ @answer.id}"
+  answer = Answer.find(params[:id])
+  vote = Vote.create(user: current_user, votable_id: params[:id], votable_type: "Answer", vote: boolean(params[:vote]) )
+    p params[:vote]
+  redirect "/questions/#{ answer.question_id}"
 end
 
 post '/comments/:id/votes' do
-  @comment = Comment.find(params[:id])
-  votes = Vote.create(user: current_user, votable_id: params[:id], votable_type: "Comment", vote: params[:vote])
-  redirect "/comments/#{ @comment.id}"
+  comment = Comment.find(params[:id])
+  vote = Vote.create(user: current_user, votable_id: params[:id], votable_type: "Comment", vote: boolean(params[:vote]) )
+  p params[:vote]
+  redirect "/questions/#{ comment.question_id}"
 end
